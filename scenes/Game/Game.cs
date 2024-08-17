2c1e4cc1b6e68cc6980e2efe816eaaac2973a6c2
JDEngi <jelledewit.engi@gmail.com>
2024-08-17 21:16:12 +0200

    Create game object for managing level and toggling UI elements. Toggle ProgramEditorUI

---
+++ scenes/Game/Game.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class Game : Node2D
+{
+
+    public void ToggleProgramEditor()
+    {
+        GetNode<Control>("MenuUI/ProgramEditor").Visible = !GetNode<Control>("MenuUI/ProgramEditor").Visible;        
+    }
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Control programEditor = GetNode<Control>("MenuUI/ProgramEditor");
+        programEditor.Visible = false;
+
+        programEditor.GetNode<Button>("MarginContainer/Panel/MarginContainer/Control/Panel/HBoxContainer/CancelButton").Pressed += ToggleProgramEditor;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed("toggle_program_editor")) {
+            ToggleProgramEditor();
+        }
+    }
+}
